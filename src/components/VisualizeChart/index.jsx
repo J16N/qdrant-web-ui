169156b639e101cbbfe3af689478f23be1425c03
J16N <jishan2001bhattacharya@gmail.com>
2024-06-30 20:36:24 +0530

    Use SharedArrayBuffer to display partial results.

--- src/components/VisualizeChart/index.jsx
+++ src/components/VisualizeChart/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 import { Button } from '@mui/material';
 import Chart from 'chart.js/auto';
 import chroma from 'chroma-js';
@@ -10,6 +12,61 @@ import { imageTooltip } from './ImageTooltip';
 import { bigIntJSON } from '../../common/bigIntJSON';
 
 const SCORE_GRADIENT_COLORS = ['#EB5353', '#F9D923', '#36AE7C'];
+
+function prepareDataset(data) {
+    const dataset = [];
+    const labelBy = data.color_by?.payload;
+    const points = data.result?.points;
+
+    if (labelBy) {
+        data.labelByArrayUnique.forEach(label => {
+            dataset.push({
+                label: label,
+                data: [],
+            })
+        })
+        points?.forEach((point) => {
+            const label = get(point.payload, labelBy);
+            dataset[data.labelByArrayUnique.indexOf(label)].data.push({
+                x: 0,
+                y: 0,
+                point: point,
+            })
+            // dataset[data.labelByArrayUnique.indexOf(label)].data.push({
+            //     x: reducedPoint[idx][0],
+            //     y: reducedPoint[idx][1],
+            //     point,
+            // })
+        })
+    }
+    else {
+        dataset.push({
+            label: 'data',
+            data: [],
+        })
+        points?.forEach((point) => {
+            dataset[0].data.push({
+                x: 0,
+                y: 0,
+                point,
+            })
+            // dataset[0].data.push({
+            //     x: reducedPoint[idx][0],
+            //     y: reducedPoint[idx][1],
+            //     point,
+            // })
+        })
+    }
+
+    return dataset;
+}
+
+function mutateDataset(dataset, reducedPoint, cols) {
+    for (let i = 0; i < reducedPoint.length / cols; ++i) {
+        dataset[0].data[i].x = reducedPoint[i * cols];
+        dataset[0].data[i].y = reducedPoint[i * cols + 1];
+    }
+}
 
 // const WASM_SUPPORTED = (() => {
 //   try {
@@ -40,245 +97,267 @@ const SCORE_GRADIENT_COLORS = ['#EB5353', '#F9D923', '#36AE7C'];
 // }
 
 const VisualizeChart = ({ scrollResult }) => {
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const [openViewPoints, setOpenViewPoints] = useState(false);
-  const [viewPoints, setViewPoint] = useState([]);
-  const action = (snackbarId) => (
-    <Button
-      variant="outlined"
-      color="inherit"
-      onClick={() => {
-        closeSnackbar(snackbarId);
-      }}
-    >
-      Dismiss
-    </Button>
-  );
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const [openViewPoints, setOpenViewPoints] = useState(false);
+    const [viewPoints, setViewPoint] = useState([]);
+    const action = (snackbarId) => (
+        <Button
+            variant="outlined"
+            color="inherit"
+            onClick={() => {
+                closeSnackbar(snackbarId);
+            }}
+        >
+            Dismiss
+        </Button>
+    );
 
-  useEffect(() => {
-    if (!scrollResult.data && !scrollResult.error) {
-      return;
-    }
-
-    if (scrollResult.error) {
-      enqueueSnackbar(`Visualization Unsuccessful\nError: ${bigIntJSON.stringify(scrollResult.error)}`, {
-        variant: 'error',
-        style: { whiteSpace: 'pre-line' },
-        action,
-      });
-      return;
-    }
-    else if (!scrollResult.data?.result?.points.length) {
-      enqueueSnackbar(`Visualization Unsuccessful\nError: No data returned`, {
-        variant: 'error',
-        style: { whiteSpace: 'pre-line' },
-        action,
-      });
-      return;
-    }
-
-    const dataset = [];
-    const colorBy = scrollResult.data.color_by;
-    const channel = new MessageChannel();
-
-    let labelby = null;
-    if (colorBy?.payload) {
-      labelby = colorBy.payload;
-      // Color and label by payload field
-      if (get(scrollResult.data.result?.points[0]?.payload, labelby) === undefined) {
-        enqueueSnackbar(`Visualization Unsuccessful\nError: Color by field ${labelby} does not exist`, {
-          variant: 'error',
-          style: { whiteSpace: 'pre-line' },
-          action,
-        });
-        return;
-      }
-      scrollResult.data.labelByArrayUnique = [
-        ...new Set(scrollResult.data.result?.points?.map((point) => get(point.payload, labelby))),
-      ];
-      scrollResult.data.labelByArrayUnique.forEach((label) => {
-        dataset.push({
-          label: label,
-          data: [],
-        });
-      });
-    } else if (colorBy?.discover_score) {
-      // Color by discover score
-      const scores = scrollResult.data.result?.points.map((point) => point.score);
-      const minScore = Math.min(...scores);
-      const maxScore = Math.max(...scores);
-
-      const colorScale = chroma.scale(SCORE_GRADIENT_COLORS);
-      const scoreColors = scores.map((score) => {
-        const normalizedScore = (score - minScore) / (maxScore - minScore);
-        return colorScale(normalizedScore).hex();
-      });
-
-      const pointRadii = scrollResult.data.result?.points.map((point) => {
-        if (point.from_query) {
-          return 4;
-        } else {
-          return 3;
+    useEffect(() => {
+        if (!scrollResult.data && !scrollResult.error) {
+            return;
         }
-      });
 
-      dataset.push({
-        label: 'Discover scores',
-        pointBackgroundColor: scoreColors,
-        pointBorderColor: scoreColors,
-        pointRadius: pointRadii,
-        data: [],
-      });
-    } else {
-      // No special coloring
-      dataset.push({
-        label: 'Data',
-        data: [],
-      });
-    }
-    const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-      type: 'scatter',
-      data: {
-        datasets: dataset,
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            grid: {
-              display: false,
-            },
-            display: false,
-          },
-          y: {
-            display: false,
-          },
-        },
-        plugins: {
-          tooltip: {
-            // only use custom tooltip if color by is not discover score
-            enabled: !colorBy?.discover_score,
-            external: (colorBy?.discover_score && imageTooltip) || undefined,
-            usePointStyle: true,
-            callbacks: {
-              label: (context) => {
-                const payload = bigIntJSON
-                  .stringify(context.dataset.data[context.dataIndex].point.payload, null, 1)
-                  .split('\n');
+        if (scrollResult.error) {
+            enqueueSnackbar(`Visualization Unsuccessful\nError: ${bigIntJSON.stringify(scrollResult.error)}`, {
+                variant: 'error',
+                style: { whiteSpace: 'pre-line' },
+                action,
+            });
+            return;
+        }
+        else if (!scrollResult.data?.result?.points.length) {
+            enqueueSnackbar(`Visualization Unsuccessful\nError: No data returned`, {
+                variant: 'error',
+                style: { whiteSpace: 'pre-line' },
+                action,
+            });
+            return;
+        }
 
-                if (colorBy?.discover_score) {
-                  const id = context.dataset.data[context.dataIndex].point.id;
-                  const score = context.dataset.data[context.dataIndex].point.score;
+        const dataset = [];
+        const colorBy = scrollResult.data.color_by;
+        const channel = new MessageChannel();
 
-                  return [`id: ${id}`, `score: ${score}`];
-                } else {
-                  return payload;
-                }
-              },
-            },
-          },
-          legend: {
-            display: !!labelby,
-          },
-        },
-      },
-      plugins: [
-        {
-          id: 'myEventCatcher',
-          beforeEvent(chart, args) {
-            const event = args.event;
-            if (event.type === 'click') {
-              if (chart.tooltip._active.length > 0) {
-                const activePoints = chart.tooltip._active.map((point) => {
-                  return {
-                    id: point.element.$context.raw.point.id,
-                    payload: point.element.$context.raw.point.payload,
-                    vector: point.element.$context.raw.point.vector,
-                  };
+        let labelby = null;
+        if (colorBy?.payload) {
+            labelby = colorBy.payload;
+            // Color and label by payload field
+            if (get(scrollResult.data.result?.points[0]?.payload, labelby) === undefined) {
+                enqueueSnackbar(`Visualization Unsuccessful\nError: Color by field ${labelby} does not exist`, {
+                    variant: 'error',
+                    style: { whiteSpace: 'pre-line' },
+                    action,
                 });
-                setViewPoint(activePoints);
-                setOpenViewPoints(true);
-              }
+                return;
             }
-          },
-        },
-        {
-          id: 'AfterUpdate',
-          afterRender: () => {
-            channel.port1.postMessage(false);
-          }
+            scrollResult.data.labelByArrayUnique = [
+                ...new Set(scrollResult.data.result?.points?.map((point) => get(point.payload, labelby))),
+            ];
+            scrollResult.data.labelByArrayUnique.forEach((label) => {
+                dataset.push({
+                    label: label,
+                    data: [],
+                });
+            });
+        } else if (colorBy?.discover_score) {
+            // Color by discover score
+            const scores = scrollResult.data.result?.points.map((point) => point.score);
+            const minScore = Math.min(...scores);
+            const maxScore = Math.max(...scores);
+
+            const colorScale = chroma.scale(SCORE_GRADIENT_COLORS);
+            const scoreColors = scores.map((score) => {
+                const normalizedScore = (score - minScore) / (maxScore - minScore);
+                return colorScale(normalizedScore).hex();
+            });
+
+            const pointRadii = scrollResult.data.result?.points.map((point) => {
+                if (point.from_query) {
+                    return 4;
+                } else {
+                    return 3;
+                }
+            });
+
+            dataset.push({
+                label: 'Discover scores',
+                pointBackgroundColor: scoreColors,
+                pointBorderColor: scoreColors,
+                pointRadius: pointRadii,
+                data: [],
+            });
+        } else {
+            // No special coloring
+            dataset.push({
+                label: 'Data',
+                data: [],
+            });
         }
-      ],
-    });
+        const ctx = document.getElementById('myChart');
+        const myChart = new Chart(ctx, {
+            type: 'scatter',
+            data: {
+                datasets: dataset,
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        grid: {
+                            display: false,
+                        },
+                        display: false,
+                    },
+                    y: {
+                        display: false,
+                    },
+                },
+                plugins: {
+                    tooltip: {
+                        // only use custom tooltip if color by is not discover score
+                        enabled: !colorBy?.discover_score,
+                        external: (colorBy?.discover_score && imageTooltip) || undefined,
+                        usePointStyle: true,
+                        callbacks: {
+                            label: (context) => {
+                                const payload = bigIntJSON
+                                    .stringify(context.dataset.data[context.dataIndex].point.payload, null, 1)
+                                    .split('\n');
 
-    const worker = new Worker(new URL('./wasmWorker.js', import.meta.url), {
-      type: 'module',
-    });
+                                if (colorBy?.discover_score) {
+                                    const id = context.dataset.data[context.dataIndex].point.id;
+                                    const score = context.dataset.data[context.dataIndex].point.score;
 
-    // if ((async () => await threads())()) {
-    //   console.log('WebAssembly supported');
-    //   worker = new Worker(new URL('./wasmWorker.js', import.meta.url), {
-    //     type: 'module',
-    //   });
-    // }
-    // else {
-    //   console.log('WebAssembly not supported');
-    //   worker = new Worker(new URL('./worker.js', import.meta.url), {
-    //     type: 'module',
-    //   });
-    // }
-
-    worker.onmessage = (m) => {
-      if (m.data.error) {
-        enqueueSnackbar(`Visualization Unsuccessful\nError: ${m.data.error}`, {
-          variant: 'error',
-          style: { whiteSpace: 'pre-line' },
-          action,
+                                    return [`id: ${id}`, `score: ${score}`];
+                                } else {
+                                    return payload;
+                                }
+                            },
+                        },
+                    },
+                    legend: {
+                        display: !!labelby,
+                    },
+                },
+            },
+            plugins: [
+                {
+                    id: 'myEventCatcher',
+                    beforeEvent(chart, args) {
+                        const event = args.event;
+                        if (event.type === 'click') {
+                            if (chart.tooltip._active.length > 0) {
+                                const activePoints = chart.tooltip._active.map((point) => {
+                                    return {
+                                        id: point.element.$context.raw.point.id,
+                                        payload: point.element.$context.raw.point.payload,
+                                        vector: point.element.$context.raw.point.vector,
+                                    };
+                                });
+                                setViewPoint(activePoints);
+                                setOpenViewPoints(true);
+                            }
+                        }
+                    },
+                },
+                {
+                    id: 'AfterUpdate',
+                    afterRender: () => {
+                        channel.port1.postMessage(false);
+                    }
+                }
+            ],
         });
-      }
-      else if (m.data.result && m.data.result.length > 0) {
-        m.data.result.forEach((dataset, index) => {
-          myChart.data.datasets[index].data = dataset.data;
+
+        const worker = new Worker(new URL('./wasmWorker.js', import.meta.url), {
+            type: 'module',
         });
-        myChart.update();
-      }
-      else {
-        enqueueSnackbar(`Visualization Unsuccessful\nError: Unexpected Error Occured`, {
-          variant: 'error',
-          style: { whiteSpace: 'pre-line' },
-          action
-        });
-      }
-    };
 
-    // Error handling for worker
-    worker.onerror = e => {
-      console.error(e);
-    }
+        // if ((async () => await threads())()) {
+        //   console.log('WebAssembly supported');
+        //   worker = new Worker(new URL('./wasmWorker.js', import.meta.url), {
+        //     type: 'module',
+        //   });
+        // }
+        // else {
+        //   console.log('WebAssembly not supported');
+        //   worker = new Worker(new URL('./worker.js', import.meta.url), {
+        //     type: 'module',
+        //   });
+        // }
 
-    if (scrollResult.data.result?.points?.length > 0) {
-      worker.postMessage({ command: "CONN" }, [channel.port2]);
-      worker.postMessage({ command: "FWD", details: scrollResult.data });
-    }
+        let sharedArray;
+        let typedArray;
+        let resultDataset;
+        const outputDim = 2;
 
-    return () => {
-      myChart.destroy();
-      worker.terminate();
-    };
-  }, [scrollResult]);
+        worker.onmessage = (m) => {
+            if (m.data.error) {
+                enqueueSnackbar(`Visualization Unsuccessful\nError: ${m.data.error}`, {
+                    variant: 'error',
+                    style: { whiteSpace: 'pre-line' },
+                    action,
+                });
+            }
+            else if (m.data.error === null) {  // m.data.result && m.data.result.length > 0) {
+                mutateDataset(resultDataset, typedArray, outputDim);
+                resultDataset.forEach((dataset, index) => {
+                    myChart.data.datasets[index].data = dataset.data;
+                });
+                myChart.update();
+            }
+            else {
+                enqueueSnackbar(`Visualization Unsuccessful\nError: Unexpected Error Occured`, {
+                    variant: 'error',
+                    style: { whiteSpace: 'pre-line' },
+                    action
+                });
+            }
+        };
 
-  return (
-    <>
-      <canvas id="myChart"></canvas>
-      <ViewPointModal openViewPoints={openViewPoints} setOpenViewPoints={setOpenViewPoints} viewPoints={viewPoints} />
-    </>
-  );
+        // Error handling for worker
+        worker.onerror = e => {
+            console.error(e);
+        }
+
+        if (scrollResult.data.result?.points?.length > 0) {
+            sharedArray = new SharedArrayBuffer(
+                Float64Array.BYTES_PER_ELEMENT * scrollResult.data.result.points.length * outputDim
+            );
+            typedArray = new Float64Array(sharedArray);
+            resultDataset = prepareDataset(scrollResult.data);
+            worker.postMessage({ command: "CONN" }, [channel.port2]);
+            worker.postMessage({
+                command: "FWD",
+                details: scrollResult.data,
+                sharedArray,
+                time: Date.now()
+            });
+        }
+
+        return () => {
+            myChart.destroy();
+            worker.terminate();
+            channel.port1.close();
+            channel.port2.close();
+        };
+    }, [scrollResult]);
+
+    return (
+        <>
+            <canvas id="myChart"></canvas>
+            <ViewPointModal
+                openViewPoints={openViewPoints}
+                setOpenViewPoints={setOpenViewPoints}
+                viewPoints={viewPoints}
+            />
+        </>
+    );
 };
 
 VisualizeChart.propTypes = {
-  scrollResult: PropTypes.object.isRequired,
+    scrollResult: PropTypes.object.isRequired,
 };
 
 export default VisualizeChart;
--- src/components/VisualizeChart/wasmWorker.js
+++ src/components/VisualizeChart/wasmWorker.js
@@ -12,7 +12,6 @@ const errorMessage = {
     error: 'No data found',
 };
 
-const MESSAGE_INTERVAL = 200;
 let port;
 let RENDERING = false;
 const sleep = ms => new Promise(r => setTimeout(r, ms));
@@ -25,13 +24,15 @@ self.onmessage = e => {
         };
         return;
     }
-    // let lastTime = new Date().getTime();
+
+    console.log(`Received data in: ${(Date.now() - e.data.time) / 1000}s`);
     const data = [];
     const points = e.data.details.result?.points;
     let vector;
     let vecName;
     let cols = 0;
     let outputDim = 2;
+    const sharedArray = new Float64Array(e.data.sharedArray);
 
     if (points?.length === 0) {
         self.postMessage(errorMessage);
@@ -43,8 +44,8 @@ self.onmessage = e => {
     }
     else if (typeof (vector = points[0].vector).length === 'number') {
         cols = vector.length;
-        // points.forEach(point => data.push(...point.vector));
-        points.forEach(point => data.push(point.vector));
+        points.forEach(point => data.push(...point.vector));
+        // points.forEach(point => data.push(point.vector));
     }
     else if (typeof vector === 'object') {
         if (!(vecName = e.data.vector_name)) {
@@ -70,8 +71,8 @@ self.onmessage = e => {
         }
 
         cols = vector[vecName].length;
-        // points.forEach(point => data.push(...point.vector[vecName]));
-        points.forEach(point => data.push(point.vector[vecName]));
+        points.forEach(point => data.push(...point.vector[vecName]));
+        // points.forEach(point => data.push(point.vector[vecName]));
     }
     else {
         errorMessage.error = 'Unexpected Error Occurred';
@@ -91,12 +92,6 @@ self.onmessage = e => {
                 console.log("Browser does not support threads");
             }
 
-            // const vectors = [];
-            // for (let i = 0; i < data.length; i += cols) {
-            //     const chunk = data.slice(i, i + cols);
-            //     vectors.push(chunk);
-            // }
-
             // set hyperparameters
             const opt = {
                 learning_rate: 150.0,
@@ -105,21 +100,25 @@ self.onmessage = e => {
             };
 
             try {
-                const tsneEncoder = new bhtSNEf64(data, opt);
+                console.time('t-SNE constructor');
+                const tsneEncoder = new bhtSNEf64(data, cols, opt);
+                console.timeEnd('t-SNE constructor');
                 for (let i = 0; i < 1000; i++) {
                     const resultPtr = tsneEncoder.step(1);
 
-                    // Give chance to other incoming messages
+                    // Give chance to other coroutines to run
                     await sleep(0);
 
                     if (RENDERING) continue;
 
                     const result = new Float64Array(memory.buffer, resultPtr, points.length * outputDim);
-                    self.postMessage({
-                        result: getDataset(e.data.details, result, outputDim),
-                        error: null,
+                    result.forEach((val, idx) => {
+                        sharedArray[idx] = val;
                     });
 
+                    self.postMessage({
+                        error: null,
+                    });
                     RENDERING = true;
                 }
             }
@@ -131,56 +130,8 @@ self.onmessage = e => {
             }
         })();
     }
-}
+};
 
 self.onerror = e => {
     console.error(e);
-}
-
-function getDataset(data, reducedPoint, cols) {
-    const dataset = [];
-    const labelBy = data.color_by?.payload;
-    const points = data.result?.points;
-
-    if (labelBy) {
-        data.labelByArrayUnique.forEach(label => {
-            dataset.push({
-                label: label,
-                data: [],
-            })
-        })
-        points?.forEach((point, idx) => {
-            const label = get(point.payload, labelBy);
-            dataset[data.labelByArrayUnique.indexOf(label)].data.push({
-                x: reducedPoint[idx * cols + 0],
-                y: reducedPoint[idx * cols + 1],
-                point: point,
-            })
-            // dataset[data.labelByArrayUnique.indexOf(label)].data.push({
-            //     x: reducedPoint[idx][0],
-            //     y: reducedPoint[idx][1],
-            //     point,
-            // })
-        })
-    }
-    else {
-        dataset.push({
-            label: 'data',
-            data: [],
-        })
-        points?.forEach((point, idx) => {
-            dataset[0].data.push({
-                x: reducedPoint[idx * cols + 0],
-                y: reducedPoint[idx * cols + 1],
-                point,
-            })
-            // dataset[0].data.push({
-            //     x: reducedPoint[idx][0],
-            //     y: reducedPoint[idx][1],
-            //     point,
-            // })
-        })
-    }
-
-    return dataset;
-}
+};
